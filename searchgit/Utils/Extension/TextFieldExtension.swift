2aa59d6c9f16f040bb1015e3becb34d7ba0fe847
Samsud Dhuha <samsudcoba@gmail.com>
2021-08-10 13:45:42 +0700

    integration view data api

--- searchgit/Utils/Extension/TextFieldExtension.swift
+++ searchgit/Utils/Extension/TextFieldExtension.swift
@@ -6,4 +6,20 @@
 //
 
 import Foundation
+import UIKit
 
+@IBDesignable
+extension UITextField{
+    
+    func setLeftPaddingPoints(_ size: CGFloat){
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: size, height: self.frame.size.height))
+        self.leftView = paddingView
+        self.leftViewMode = .always
+    }
+    
+    func setRightPaddingPoints(_ size: CGFloat) {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: size, height: self.frame.size.height))
+        self.rightView = paddingView
+        self.rightViewMode = .always
+    }
+}
--- searchgit/Utils/Extension/ViewExtension.swift
+++ searchgit/Utils/Extension/ViewExtension.swift
@@ -17,4 +17,13 @@ extension UIView {
         self.layer.mask = mask
     }
     
+    func dropShadow(color: UIColor = UIColor.lightGray, opacity: Float = 0.3, offSet: CGSize =  CGSize(width: -1, height: 1), radius: CGFloat = 3, scale: Bool = true) {
+        layer.masksToBounds = false
+        layer.shadowColor = color.cgColor
+        layer.shadowOpacity = opacity
+        layer.shadowOffset = offSet
+        layer.shadowRadius = radius
+        
+      }
+    
 }
